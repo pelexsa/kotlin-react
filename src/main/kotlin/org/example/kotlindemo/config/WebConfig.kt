03ab3033759c2a3156533aa612a788d770c3d614
wwl.hmh <hmh@winwinlab.co.kr>
2024-01-02 17:37:48 +0900

    [add] cors 처리

---
+++ src/main/kotlin/org/example/kotlindemo/config/WebConfig.kt
@@ -0,0 +1,21 @@
+package org.example.kotlindemo.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedHeaders(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                )
+    }
+
+}
--- src/main/kotlin/org/example/kotlindemo/web/MainController.kt
+++ src/main/kotlin/org/example/kotlindemo/web/MainController.kt
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class MainController {
